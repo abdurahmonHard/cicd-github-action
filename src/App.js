d87b7a5214aea866ff619d27c382e2c7de24146b
abdurahmonHard <abdurahmonusmonov633@gmail.com>
2023-07-06 15:37:35 +0500

    second

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+//         ci cd ni vazifasi yangi kod yozilganda yangi kod yetib kelganda 
+//         o'sha kodni and yuzer foydalanuchiga tug'ridan tug'ri yetkazib berishni aftomatlashtirish ci cd deyiladi
+//         bu jarayon ichiga eslint, yunit testing, integretion testing,  countinion integration bo'ladi countinion delevery qismiga o'tadi -->
+//         countinion delevery qismi ham qaysi tehnalogiya ligiga qarab yoki kod deploy qilinayotgan server turiga qarab har hil boladi 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
